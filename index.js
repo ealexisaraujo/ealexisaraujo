9cfefe14c18f325c5dfea2c902d08a4acb56b2c6
Alexis Araujo <ala1289@gmail.com>
2020-07-13 16:57:52 -0500

    Agregar dotenv y CORS

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const app = require('./app');
 const { createConnection } = require('./db');
+const cors = require('cors');
+
+app.use(cors());
+app.use(express.json());
 
 createConnection();
-app.listen(3000);
-console.log('Server on port', 3000);
+app.listen(config.port, function () {
+  console.log(`Listening http://localhost:${config.port}`);
+});
